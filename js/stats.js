bf096150cad95a7d324a6fe2b90ae3c0d687e5cc
Renaud Schleck <renaud.schleck@free.fr>
2024-01-13 23:46:44 +0100

    13/01/2024

--- js/stats.js
+++ js/stats.js
@@ -11,6 +11,29 @@ function send_click(data){
 }
 
 
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 function test_click(){
 data = {'width':1, 'height':3, 'useragent':'testua', 'location':'ici', "fingerprint":"1234"}
   console.log(data);
@@ -21,9 +44,10 @@ let width ;
 let height ;
 let ua ; 
 let fingerprint = "undef";
+let uuid = getCookie("visitor_id");
 
 function send_initial_click(){
-  send_click({'width':width, 'height':height, 'useragent':ua, 'location':window.location.toLocaleString(), "fingerprint":fingerprint}) ;
+  send_click({'width':width, 'height':height, 'useragent':ua, 'location':window.location.toLocaleString(), "fingerprint":fingerprint, "uuid":uuid}) ;
 }
 
 
@@ -32,6 +56,10 @@ window.addEventListener("load", (event) => {
   height = window.screen.availHeight
   ua = window.navigator.userAgent
   fingerprint = new Fingerprint().get().toString(16).toUpperCase();
+  if(uuid == ""){
+    uuid = crypto.randomUUID();
+    setCookie("visitor_id", uuid, 730);
+  }
   setTimeout(send_initial_click, 100);
 
 
@@ -41,7 +69,7 @@ window.addEventListener("load", (event) => {
       if (e.target.tagName !== 'A')
           return;
 
-      send_click({'width':width, 'height':height, 'useragent':ua, 'location':e.target.href, "fingerprint":fingerprint})
+      send_click({'width':width, 'height':height, 'useragent':ua, 'location':e.target.href, "fingerprint":fingerprint, "uuid":uuid})
   }
 
   if (document.addEventListener)
